ntzwrk/blockstack.ts

commit ae6bc6015fe2e3c5edbb414badc2f74c97e2b7b2

Fix potential doubly buffered input

src/encryption.ts
@@ -62,7 +62,11 @@ export function getHexFromBN(bnInput: Object) {
  */
 export function encryptECIES(publicKey: string, content: string | Buffer) {
   const isString = (typeof(content) === 'string')
-  const plainText = new Buffer(content) // always copy to buffer
+  if(isString) {
+    const plainText = new Buffer(<string>content)
+  } else {
+    const plainText = content
+  }
 
   const ecPK = ecurve.keyFromPublic(publicKey, 'hex').getPublic()
   const ephemeralSK = ecurve.genKeyPair()

commit d677b248e257ff861bd004c042fa5fe17fa5de2a

Fix error introduced in ae6bc6015fe2e3c5edbb414badc2f74c97e2b7b2

src/encryption.ts
@@ -62,10 +62,11 @@ export function getHexFromBN(bnInput: Object) {
  */
 export function encryptECIES(publicKey: string, content: string | Buffer) {
   const isString = (typeof(content) === 'string')
+  var plainText
   if(isString) {
-    const plainText = new Buffer(<string>content)
+    plainText = new Buffer(<string>content)
   } else {
-    const plainText = content
+    plainText = content
   }
 
   const ecPK = ecurve.keyFromPublic(publicKey, 'hex').getPublic()