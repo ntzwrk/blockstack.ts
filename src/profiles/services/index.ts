df170103e704b609a9e7686c698b28673496d158
Valentin Sundermann <me@vsund.de>
2018-03-24 10:44:15 +0100

    Make the supported services a proper Map

--- src/profiles/services/index.ts
+++ src/profiles/services/index.ts
@@ -4,14 +4,15 @@ import { HackerNews } from './hackerNews';
 import { Instagram } from './instagram';
 import { LinkedIn } from './linkedIn';
 import { Twitter } from './twitter';
+import { Service } from './service';
 
-export const profileServices = {
-	facebook: Facebook,
-	github: Github,
-	hackerNews: HackerNews,
-	instagram: Instagram,
-	linkedIn: LinkedIn,
-	twitter: Twitter
-};
+export const profileServices: Map<string, Service> = new Map([
+	['facebook', Facebook],
+	['github', Github],
+	['hackerNews', HackerNews],
+	['instagram', Instagram],
+	['linkedIn', LinkedIn],
+	['twitter', Twitter]
+]);
 
 export { containsValidProofStatement, containsValidAddressProofStatement, IProof } from './serviceUtils';
