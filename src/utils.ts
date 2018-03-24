54df2718877ce2e0fdf5a546f3fb6eb23eb0e42f
Valentin Sundermann <me@vsund.de>
2018-03-24 10:37:16 +0100

    Ignore bitwise operations

--- src/utils.ts
+++ src/utils.ts
@@ -100,9 +100,9 @@ export function makeUUID4() {
 		d += performance.now(); // use high-precision timer if available
 	}
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-		const r = ((d + Math.random() * 16) % 16) | 0;
+		const r = ((d + Math.random() * 16) % 16) | 0; // tslint:disable-line
 		d = Math.floor(d / 16);
-		return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+		return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16); // tslint:disable-line
 	});
 }
 
