68da11b16f8032143d97acf2e2bc0130db8a093e
Valentin Sundermann <me@vsund.de>
2018-03-26 15:45:34 +0200

    Remove legacy profile conversion method into Person

--- src/profile/Person.ts
+++ src/profile/Person.ts
@@ -6,7 +6,6 @@ import { PostalAddressJson } from './schema/components/PostalAddress.json';
 import { WebSiteJson } from './schema/components/WebSite.json';
 import { PersonJson } from './schema/Person.json';
 import { PersonLegacyJson } from './schema/PersonLegacy.json';
-import { getPersonFromLegacyFormat } from './utils/personLegacy';
 
 export interface IVerification {
 	identifier: string;
@@ -17,12 +16,129 @@ export interface IVerification {
 
 export class Person extends Profile implements PersonJson {
 	public static fromLegacyFormat(legacyProfileJson: PersonLegacyJson) {
-		const profileJson = getPersonFromLegacyFormat(legacyProfileJson);
+		const profileJson = Person.getPersonFromLegacyFormat(legacyProfileJson);
 		return Person.fromJSON(profileJson);
 	}
 
 	public static fromJSON(personJson: PersonJson): Person {
 		return new Person(personJson['@id']);
+	}
+
+	private static formatAccount(
+		serviceName: string,
+		data: {
+			username?: string | undefined;
+			proof?: { url?: string | undefined } | undefined;
+		}
+	) {
+		let proofUrl;
+		if (data.proof !== undefined && data.proof.url !== undefined) {
+			proofUrl = data.proof.url;
+		}
+		return {
+			'@type': 'Account',
+			identifier: data.username,
+			proofType: 'http',
+			proofUrl: proofUrl,
+			service: serviceName
+		};
+	}
+
+	private static getPersonFromLegacyFormat(personLegacyJson: PersonLegacyJson): PersonJson {
+		const personJson: PersonJson = {
+			'@context': 'http://schema.org/',
+			'@id': '', // TODO: What should @id be? Name?
+			'@type': 'Person'
+		};
+
+		if (personLegacyJson.name !== undefined) {
+			personJson.name = personLegacyJson.name.formatted;
+		}
+
+		personJson.description = personLegacyJson.bio;
+
+		if (personLegacyJson.location !== undefined) {
+			personJson.address = {
+				'@type': 'PostalAddress',
+				addressLocality: personLegacyJson.location.formatted
+			};
+		}
+
+		const images: ImageJson[] = [];
+		if (personLegacyJson.avatar !== undefined && personLegacyJson.avatar.url !== undefined) {
+			images.push({
+				'@type': 'ImageObject',
+				contentUrl: personLegacyJson.avatar.url,
+				name: 'avatar'
+			});
+		}
+		if (personLegacyJson.cover !== undefined && personLegacyJson.cover.url !== undefined) {
+			images.push({
+				'@type': 'ImageObject',
+				contentUrl: personLegacyJson.cover.url,
+				name: 'cover'
+			});
+		}
+		if (images !== []) {
+			personJson.image = images;
+		}
+
+		if (personLegacyJson.website !== undefined) {
+			personJson.website = [
+				{
+					'@type': 'WebSite',
+					url: personLegacyJson.website
+				}
+			];
+		}
+
+		const accounts: AccountJson[] = [];
+		if (personLegacyJson.bitcoin !== undefined && personLegacyJson.bitcoin.address !== undefined) {
+			accounts.push({
+				'@type': 'Account',
+				identifier: personLegacyJson.bitcoin.address,
+				role: 'payment',
+				service: 'bitcoin'
+			});
+		}
+		if (personLegacyJson.twitter !== undefined && personLegacyJson.twitter.username !== undefined) {
+			accounts.push(Person.formatAccount('twitter', personLegacyJson.twitter));
+		}
+		if (personLegacyJson.facebook !== undefined && personLegacyJson.facebook.username !== undefined) {
+			accounts.push(Person.formatAccount('facebook', personLegacyJson.facebook));
+		}
+		if (personLegacyJson.github !== undefined && personLegacyJson.github.username !== undefined) {
+			accounts.push(Person.formatAccount('github', personLegacyJson.github));
+		}
+		if (personLegacyJson.auth !== undefined) {
+			if (personLegacyJson.auth.length > 0) {
+				if (personLegacyJson.auth[0].publicKeychain !== undefined) {
+					accounts.push({
+						'@type': 'Account',
+						identifier: personLegacyJson.auth[0].publicKeychain,
+						role: 'key',
+						service: 'bip32'
+					});
+				}
+			}
+		}
+		if (
+			personLegacyJson.pgp !== undefined &&
+			personLegacyJson.pgp.fingerprint !== undefined &&
+			personLegacyJson.pgp.url !== undefined
+		) {
+			accounts.push({
+				'@type': 'Account',
+				contentUrl: personLegacyJson.pgp.url,
+				identifier: personLegacyJson.pgp.fingerprint,
+				role: 'key',
+				service: 'pgp'
+			});
+		}
+
+		personJson.account = accounts;
+
+		return personJson;
 	}
 
 	constructor(
