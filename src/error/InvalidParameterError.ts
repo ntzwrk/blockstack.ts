7beddaa01400bbfa73d4c6271beb55ebba18a6b2
Valentin Sundermann <me@vsund.de>
2018-03-27 17:05:07 +0200

    Move already existing errors into new structure

--- src/error/InvalidParameterError.ts
+++
@@ -1,8 +0,0 @@
-import { BlockstackError } from './index';
-
-export class InvalidParameterError extends BlockstackError {
-	constructor(parameter: string, message: string = '') {
-		super({ code: 'missing_parameter', message, parameter: '' });
-		this.name = 'MissingParametersError';
-	}
-}
