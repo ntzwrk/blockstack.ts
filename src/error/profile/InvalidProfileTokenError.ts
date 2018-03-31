04249f9bbea61312eea8677df037cb4584a2f23c
Valentin Sundermann <me@vsund.de>
2018-03-31 22:14:28 +0200

    Let InvalidProfileTokenError also take ProfileTokenJson

--- src/error/profile/InvalidProfileTokenError.ts
+++ src/error/profile/InvalidProfileTokenError.ts
@@ -1,11 +1,13 @@
 import { JWT } from 'jsontokens';
 
+import { ProfileTokenJson } from '../../profile/schema/ProfileToken.json';
+
 export class InvalidProfileTokenError extends Error {
 	public readonly name: string = 'InvalidProfileTokenError';
 	public readonly message: string;
-	public readonly profileToken: string | JWT;
+	public readonly profileToken: string | JWT | ProfileTokenJson;
 
-	constructor(profileToken: string | JWT, message?: string) {
+	constructor(profileToken: string | JWT | ProfileTokenJson, message?: string) {
 		super(message);
 
 		this.message = message !== undefined ? message : 'Could not verify the given profile token';
