8d81f1ae23e1876f724033aa4382ea4f17ec9938
Valentin Sundermann <me@vsund.de>
2018-04-21 23:16:34 +0200

    Add default core client

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
+import { BlockstackCoreClient } from 'blockstack-core-client.ts';
+
 import { BlockstackNetwork, defaults } from './operation/network';
 
-export const config = { network: defaults.MAINNET_DEFAULT };
+export const config = {
+	coreClient: new BlockstackCoreClient('core.blockstack.org'),
+	network: defaults.MAINNET_DEFAULT
+};
