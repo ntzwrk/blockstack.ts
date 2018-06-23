3e671bdb99c6be0165be793fddbf1817bc513e7d
Valentin Sundermann <me@vsund.de>
2018-06-23 12:08:09 +0200

    Rename ProfileZoneFile to NameZoneFile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './auth';
 export * from './crypto';
+export * from './NameZoneFile';
 export * from './operation';
 export * from './profile';
-export * from './ProfileZoneFile';
 export * from './storage';
